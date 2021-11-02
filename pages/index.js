cb410bb5477912c2fe20544ec7b62b2e34bd16e7
GaurangPatel <gp853212@gmail.com>
2021-11-02 22:34:02 +0530

    Add Header And Button Styled Component

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home = () => {
-  return (
-    <Layout>
-      <Section grid>
-        <Hero />
-        <BgAnimation />
-      </Section>
-      <Projects />
-      <Technologies />
-      <Timeline />
-      <Acomplishments />
-    </Layout>
-  );
-};
+// const Home = () => {
+//   return (
+//     <Layout>
+//       <Section grid>
+//         <Hero />
+//         <BgAnimation />
+//       </Section>
+//       <Projects />
+//       <Technologies />
+//       <Timeline />
+//       <Acomplishments />
+//     </Layout>
+//   );
+// };
